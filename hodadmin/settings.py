cdf20ee52194d71651308a552617a604e5cb1134
ajnasajnas1234 <mubashiraajnas@gmail.com>
2023-09-17 23:49:41 +0530

    requirements

--- hodadmin/settings.py
+++ hodadmin/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = "hodadmin.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "hod/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "hod/static/"
+STATIC_URL = "/static/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
